00347b6ba9acacbfa0b6d78ffbad34dfc972ae05
SadeqSp <sadeq.sp@gmail.com>
2021-04-28 14:36:12 +0430

    4-conditional-rendering

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: "Socks",
-            image: "./assets/g.jpg"
+            image: "./assets/g.jpg",
+            inventory: 8
         }
     }
 })
