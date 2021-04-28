9387a5dad443c1b9ba595fe27bd656f9a6dfb3ac
SadeqSp <sadeq.sp@gmail.com>
2021-04-28 14:44:14 +0430

    7-class-and-style-binding

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: "Socks",
             image: "./assets/g.jpg",
-            inventory: 8,
+            inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 { id: 2234, color: 'green', image: './assets/g.jpg' },
