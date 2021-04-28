fa970a5c221191a161f7dd220e93480d358c4103
SadeqSp <sadeq.sp@gmail.com>
2021-04-28 14:58:16 +0430

    9-components-and-props

---
+++ components/productDisplay.js
@@ -0,0 +1,78 @@
+app.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: /*html*/ `
+        <div class="product-display">
+            <div class="product-container">
+                <div class="product-image">
+                    <img v-bind:src="image" :class="{'out-of-stock-img': !inStock}">
+                </div>
+                <div class="product-info">
+                    <h1>{{ title }}</h1>
+
+                    <p v-if="inStock">In Stock</p>
+                    <p v-else>Out of Stock</p>
+
+                    <p>Shipping: {{ shipping }}</p>
+
+                    <ul><li v-for="detail in details">{{ detail }}</li></ul>
+                    
+                    <div 	v-for="(variant, index) in variants" 
+                            :key="variant.id"
+                            class="color-circle"
+                            :style="{backgroundColor: variant.color}" 
+                            @mouseover="updateVariant(index)">
+                    </div>
+
+                    <button class="button" 
+                            :disabled="!inStock"
+                            :class="{disabledButton: !inStock}"
+                            v-on:click="addToCart">
+                            Add to Cart
+                    </button>
+                </div>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            product: "Socks",
+            brand: "Vue Mastery",
+            selectedVariant: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/g.jpg', quantity: 50 },
+                { id: 2235, color: 'blue',  image: './assets/b.jpg',  quantity: 0  },
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        shipping() {
+            if(this.premium) {
+                return "free"
+            }
+            return 2.99
+        }
+    }
+})
