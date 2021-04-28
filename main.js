313954676cf6aec34895846baaa7f08be0daeb37
SadeqSp <sadeq.sp@gmail.com>
2021-04-28 14:37:53 +0430

    5-list-rendering

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const app = Vue.createApp({
         return {
             product: "Socks",
             image: "./assets/g.jpg",
-            inventory: 8
+            inventory: 8,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ]
         }
     }
 })
